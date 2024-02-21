92b986c560b3e0f33ec37e7fd8603187518f271a
parkbjgit <92544688+parkbjgit@users.noreply.github.com>
2024-02-21 16:12:56 +0900

    modify

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
         <div>
             <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
             <img src={coverImg}/>
-            <p>{summary}</p>
+            <p>{summary.length>235 ? `${summary.slice(0,235)}...`: summary}</p>
             <ul>
                 {genres.map((g) => (
                     <li key={g}>{g}</li>))}
